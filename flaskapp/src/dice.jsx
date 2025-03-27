e392e75e313a3ad1b20d825d612da5d672c2ee66
Richard Hildred <rhildred@gmail.com>
2025-03-27 20:05:17 +0000

    change traceparent to match format

--- flaskapp/src/dice.jsx
+++ flaskapp/src/dice.jsx
@@ -7,7 +7,7 @@ export default function Dice(){
             "dice-five-svgrepo-com.svg", "dice-six-svgrepo-com.svg"
         ];
         fetch("https://ideal-winner-75w6v6pw7hw4rj-5000.app.github.dev/rolldice", {headers:{
-            traceparent: "123-456-789-123"
+            traceparent: "00-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7-00"
         }}).then((res)=>{
             res.text().then((sSide)=>{
                 setFace(sides[sSide - 1]);
